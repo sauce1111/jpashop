9ebd1fd5c9474c5ec183330e70a2606928f8ef17
Park seon-seo <44424542+sauce1111@users.noreply.github.com>
2020-09-24 16:47:14 +0900

    type : fix 양방향 연관관계의 한쪽에 @JsonIgnore 추가

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -2,12 +2,12 @@ package jpabook.jpashop.domain;
 
 import static javax.persistence.FetchType.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
@@ -24,6 +24,7 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
+    @JsonIgnore
     @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 
